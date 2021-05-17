5781d83348db95c03b03880376881f05dbf31615
louloubadillo <lulu.badillo.csm@gmail.com>
2021-05-17 16:55:16 -0500

    carrousel ideas

--- carrousel2.js
+++ carrousel2.js
@@ -14,12 +14,7 @@ class Carrousel{
         //console.log(this.children);
         this.dom.addEventListener('scroll', ()=>{
             this.children.forEach((child, i) => {
-                if(this.isElementVisible(child)){
-                    child.classList.add('visible');
-                    this.activeIndex = i;
-                }else{
-                    child.classList.remove('visible');
-                }
+                //nth
             });
             console.log(this.children.map(c=>this.isElementVisible(c)), this.activeIndex)
         });
@@ -29,32 +24,22 @@ class Carrousel{
     /**
      * @param {HTMLElement} element;
      */
-    isElementVisible(element){
-        let parent = element.parentElement;
-        let parentRect = parent.getBoundingClientRect();
-        let elementRect = element.getBoundingClientRect();
-        let rect = {
-            top:    elementRect.top - parentRect.top,
-            bottom: parentRect.bottom - elementRect.bottom,
-            left:   elementRect.left - parentRect.left,
-            right:  - parentRect.right + elementRect.right
-        }
-        return Object.values(rect).every(item=>item==0);
-    }
+
     goToPrev(){
-        console.log(this.activeIndex)
         if(this.activeIndex==0){
             this.dom.scrollLeft = this.children[this.children.length-1].offsetLeft;
         } else {
             this.dom.scrollLeft = this.children[0].offsetLeft;
+            activeIndex--;
         }
     }
     goToNext(){
-        console.log(this.activeIndex)
         if(this.activeIndex==this.children.length-1){
             this.dom.scrollLeft = this.children[0].offsetLeft;
+            this.activeIndex = 0; 
         } else {
             this.dom.scrollLeft = this.children[this.activeIndex+1].offsetLeft;
+            this.activeIndex++;
         }
     }
 }
